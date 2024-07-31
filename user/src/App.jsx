e348d955a1ddfbb6565a15227b9d78efb1a547f1
monirulHaque <monirul.haque@g.bracu.ac.bd>
2024-07-31 21:50:38 +0600

    Added routes and pages

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+
 
 export default function App() {
   return (
-    <div>Hello World!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashBoard" element={<Dashboard />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
